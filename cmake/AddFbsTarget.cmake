47d0da35f552d1fa2ca5d489990e200ba25b9ede
Olivier LDff <olivier.ldff@gmail.com>
2020-02-17 14:17:44 +0100

    Add new option PACK_SCHEMA to generate a C header with the schema file embedded into a byte array. This is useful to parse json from schema

---
+++ cmake/AddFbsTarget.cmake
@@ -0,0 +1,162 @@
+include(CMakeParseArguments)
+
+function(add_fbs_target TARGET SOURCES)
+
+    set(FBS_OPTIONS VERBOSE
+        PACK_SCHEMAS
+        )
+    set(FBS_ONE_VALUE_ARG
+        GENERATED_INCLUDE_DIR
+        BINARY_SCHEMA_DIR
+        COPY_TEXT_SCHEMA_DIR
+        )
+    set(FBS_MULTI_VALUE_ARG
+        DEPENDENCIES
+        FLATC_ARGUMENTS
+        INCLUDE_DIR
+        )
+     # parse the function arguments
+    cmake_parse_arguments(ARGFBS "${FBS_OPTIONS}" "${FBS_ONE_VALUE_ARG}" "${FBS_MULTI_VALUE_ARG}" ${ARGN})
+
+    set(FBS_TARGET ${TARGET})
+    set(FBS_SRCS ${SOURCES})
+    set(FBS_VERBOSE ${ARGFBS_VERBOSE})
+    set(FBS_PACK_SCHEMAS ${ARGFBS_PACK_SCHEMAS})
+    set(FBS_GENERATED_INCLUDE_DIR ${ARGFBS_GENERATED_INCLUDE_DIR})
+    set(FBS_BINARY_SCHEMA_DIR ${ARGFBS_BINARY_SCHEMA_DIR})
+    set(FBS_COPY_TEXT_SCHEMA_DIR ${ARGFBS_COPY_TEXT_SCHEMA_DIR})
+    set(FBS_DEPENDENCIES ${ARGFBS_DEPENDENCIES})
+    set(FBS_FLATC_ARGUMENTS ${ARGFBS_FLATC_ARGUMENTS})
+    set(FBS_INCLUDE_DIR ${ARGFBS_INCLUDE_DIR})
+
+    # Print verbose parameters for easy debugging
+    if(FBS_VERBOSE)
+
+        message(STATUS "Add Flat Buffers generated target : ${TARGET}")
+        message(STATUS "FBS_TARGET                : ${FBS_TARGET}")
+        message(STATUS "FBS_SRCS                  : ${FBS_SRCS}")
+        message(STATUS "FBS_VERBOSE               : ${FBS_VERBOSE}")
+        message(STATUS "FBS_PACK_SCHEMAS          : ${FBS_PACK_SCHEMAS}")
+        message(STATUS "FBS_GENERATED_INCLUDE_DIR : ${FBS_GENERATED_INCLUDE_DIR}")
+        message(STATUS "FBS_BINARY_SCHEMA_DIR     : ${FBS_BINARY_SCHEMA_DIR}")
+        message(STATUS "FBS_COPY_TEXT_SCHEMA_DIR  : ${FBS_COPY_TEXT_SCHEMA_DIR}")
+        message(STATUS "FBS_DEPENDENCIES          : ${FBS_DEPENDENCIES}")
+        message(STATUS "FBS_FLATC_ARGUMENTS       : ${FBS_FLATC_ARGUMENTS}")
+        message(STATUS "FBS_INCLUDE_DIR           : ${FBS_INCLUDE_DIR}")
+
+    endif(FBS_VERBOSE)
+
+    # ALl the generated created by this function
+    set(ALL_GENERATED_FILES "")
+    set(INCLUDE_PARAMS "")
+
+    if(FBS_PACK_SCHEMAS)
+        if(NOT TARGET flat2h)
+            if(FBS_VERBOSE)
+                message(STATUS "Create flat2h from ${FLATBUFFERS_CMAKE_ROOT}/src/flat2h.cpp")
+            endif()
+            add_executable(flat2h ${FLATBUFFERS_CMAKE_ROOT}/src/flat2h.cpp)
+            set_target_properties(flat2h
+                PROPERTIES FOLDER ${FBS_FOLDER_PREFIX})
+            if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+                target_compile_definitions(flat2h PRIVATE -D_CRT_SECURE_NO_WARNINGS)
+            endif()
+        endif()
+    endif()
+
+    # Generate the include file param
+    # The form is -I path/to/dir1 -I path/to/dir2 etc...
+    foreach (INCLUDE_DIR ${FBS_INCLUDE_DIR})
+
+        set(INCLUDE_PARAMS -I ${INCLUDE_DIR} ${INCLUDE_PARAMS})
+
+    endforeach()
+
+    foreach(SRC ${FBS_SRCS})
+
+        # Isolate filename to create the generated filename
+        get_filename_component(FILENAME ${SRC} NAME_WE)
+
+        # We check that we have an output directory before generating a rule
+        if (NOT ${FBS_GENERATED_INCLUDE_DIR} STREQUAL "")
+
+            # Name of the output generated file
+            set(GENERATED_INCLUDE ${FBS_GENERATED_INCLUDE_DIR}/${FILENAME}_generated.h)
+
+            # Add the rule for each files
+            message(STATUS "Add rule to build ${FILENAME}_generated.h from ${SRC}")
+            add_custom_command(
+                OUTPUT ${GENERATED_INCLUDE}
+                COMMAND flatc ${FBS_FLATC_ARGUMENTS}
+                -o ${FBS_GENERATED_INCLUDE_DIR}
+                ${INCLUDE_PARAMS}
+                -c ${SRC}
+                DEPENDS flatc ${SRC} ${FBS_DEPENDENCIES}
+                COMMENT "Generate ${GENERATED_INCLUDE} from ${SRC}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+            list(APPEND ALL_GENERATED_FILES ${GENERATED_INCLUDE})
+
+            if(FBS_PACK_SCHEMAS)
+
+                # Name of the output generated file
+                set(GENERATED_PACK ${FBS_GENERATED_INCLUDE_DIR}/${FILENAME}_pack.h)
+
+                message(STATUS "Add rule to build ${GENERATED_PACK} from ${SRC}")
+                add_custom_command(
+                    OUTPUT ${GENERATED_PACK}
+                    COMMAND flat2h
+                    -i ${SRC}
+                    -o ${GENERATED_PACK}
+                    DEPENDS flat2h ${SRC} ${FBS_DEPENDENCIES}
+                    COMMENT "Generate ${GENERATED_PACK} from ${SRC}"
+                    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+                list(APPEND ALL_GENERATED_FILES ${GENERATED_PACK})
+            endif()
+
+        endif() # (NOT ${FBS_GENERATED_INCLUDE_DIR} STREQUAL "")
+
+        # Should we also build bfbs
+        if (NOT ${FBS_BINARY_SCHEMA_DIR} STREQUAL "")
+
+            # Name of the output binary buffer file
+            set(BINARY_SCHEMA ${FBS_BINARY_SCHEMA_DIR}/${FILENAME}.bfbs)
+            message(STATUS "Add rule to build ${FILENAME}.bfbs from ${SRC}")
+            add_custom_command(
+                OUTPUT ${BINARY_SCHEMA}
+                COMMAND flatc -b --schema
+                -o ${FBS_BINARY_SCHEMA_DIR}
+                ${INCLUDE_PARAMS}
+                ${SRC}
+                DEPENDS ${FLATC_TARGET} ${SRC} ${FBS_DEPENDENCIES}
+                COMMENT "Generate ${FILENAME}.bfbs in ${FBS_GENERATED_INCLUDE_DIR} from ${SRC}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+            list(APPEND ALL_GENERATED_FILES ${BINARY_SCHEMA})
+
+        endif() # (NOT ${FBS_BINARY_SCHEMA_DIR} STREQUAL "")
+
+        if (NOT ${FBS_COPY_TEXT_SCHEMA_DIR} STREQUAL "")
+
+            # Name of the output binary buffer file
+            set(COPY_SCHEMA ${FBS_COPY_TEXT_SCHEMA_DIR}/${FILENAME}.fbs)
+            message(STATUS "Add rule to copy ${FILENAME}.fbs")
+            add_custom_command(
+                OUTPUT ${COPY_SCHEMA}
+                COMMAND ${CMAKE_COMMAND} -E copy ${SRC} ${COPY_SCHEMA}
+                DEPENDS ${FLATC_TARGET} ${SRC} ${FBS_DEPENDENCIES}
+                COMMENT "Copy file ${SRC} to ${COPY_SCHEMA}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+            list(APPEND ALL_GENERATED_FILES ${COPY_SCHEMA})
+        endif() # (NOT ${FBS_COPY_TEXT_SCHEMA_DIR} STREQUAL "")
+
+    endforeach() # SRC ${FBS_SRCS}
+
+    if(FBS_VERBOSE)
+        message(STATUS "${FBS_TARGET} generated files : ${ALL_GENERATED_FILES}")
+    endif(FBS_VERBOSE)
+
+    add_custom_target(${FBS_TARGET}
+        DEPENDS flatc ${ALL_GENERATED_FILES} ${FBS_DEPENDENCIES}
+    )
+
+
+endfunction()
---
+++ src/flat2h.cpp
@@ -0,0 +1,208 @@
+#include <vector>
+#include <string>
+#include <regex>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32738
+
+static void print_help(const char* name)
+{
+    printf("OVERVIEW: Converts flatbuffers schema .fbs to C header\n\n");
+    printf("USAGE: %s -i <input> -o <output> -n <name> -d -z\n\n", name);
+    printf("OPTIONS:\n");
+    printf("    -i <input>      Specify input file\n");
+    printf("    -o <output>     Specify output file\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int result = EXIT_FAILURE;
+    int i = 0;
+    int arg;
+    FILE* input_f = NULL;
+    FILE* output_f = NULL;
+    const char* input = NULL;
+    const char* output = NULL;
+    std::string s;
+    std::smatch m;
+    std::regex e("([\\w-]+)\\.fbs");
+    std::string className;
+
+    int byte;
+    
+    std::string guard = "__";
+    char str[BUFFER_SIZE];
+    char* pos;
+    std::vector<std::string> namespaces;
+
+    if (argc <= 1 ||
+        (argc == 2 && strcmp(argv[1], "-h") == 0))
+    {
+        print_help((argc > 0) ? argv[0] : "flat2h");
+        return EXIT_SUCCESS;
+    }
+
+    for (arg = 1; arg < argc; ++arg)
+    {
+        if (strcmp(argv[arg], "-i") == 0)
+        {
+            if (++arg >= argc)
+            {
+                printf("Missing argument for -i\n");
+                return EXIT_FAILURE;
+            }
+            input = argv[arg];
+        }
+        else if (strcmp(argv[arg], "-o") == 0)
+        {
+            if (++arg >= argc)
+            {
+                printf("Missing argument for -o\n");
+                return EXIT_FAILURE;
+            }
+            output = argv[arg];
+        }
+        else
+        {
+            printf("Invalid argument: %s\n", argv[arg]);
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (!input)
+    {
+        printf("No input file given\n");
+        goto exit;
+    }
+
+    if (!output)
+    {
+        printf("No output file given\n");
+        goto exit;
+    }
+
+    input_f = fopen(input, "rb");
+
+    if (!input_f)
+    {
+        printf("Failed to open input file\n");
+        goto exit;
+    }
+
+    output_f = fopen(output, "w");
+
+    if (!output_f)
+    {
+        printf("Failed to open output file\n");
+        goto exit;
+    }
+
+    while (namespaces.empty() && (fgets(str, BUFFER_SIZE, input_f) != NULL))
+    {
+        // Find next occurrence of word in str
+        if ((pos = strstr(str, "namespace")) != NULL)
+        {
+            const char* separators = " .;";
+
+            // Look for every namespace
+            char* strToken = strtok(pos, separators);
+
+            while (strToken != NULL)
+            {
+                if (strToken[0] == '\n' || strToken[0] == '\r')
+                {
+                    break;
+                }
+                if (strcmp(strToken, "namespace") != 0)
+                {
+                    namespaces.emplace_back(strToken);
+                }
+                strToken = strtok(NULL, separators);
+            }
+        }
+    }
+
+    s = std::string(input);
+    if (std::regex_search(s, m, e))
+    {
+        if (m.size() <= 1)
+        {
+            printf("Failed to parse class name\n");
+            goto exit;
+        }
+        className = m[1].str();
+    }
+
+    for (const auto& it : namespaces)
+    {
+        std::string upper(it);
+        for (auto& c : upper) c = toupper(c);
+        guard += upper;
+        guard += "_";
+    }
+    {
+        std::string upper(className);
+        for (auto& c : upper) c = toupper(c);
+        guard += upper;
+        guard += "_";
+    }
+    guard += "H__";
+
+    fseek(input_f, 0, SEEK_SET);
+    fprintf(output_f, "#ifndef %s\n", guard.c_str());
+    fprintf(output_f, "#define %s\n\n", guard.c_str());
+    fprintf(output_f, "/** File generated with flat2h, do not modify. */\n\n");
+    for (const auto& it : namespaces)
+        fprintf(output_f, "namespace %s {\n", it.c_str());
+
+    fprintf(output_f, "\nclass %sSchema \n{\npublic:\n", className.c_str());
+
+    fprintf(output_f, "    static const char* data()\n    {\n");
+
+    fprintf(output_f, "        static const char d[] = \n        {\n        ");
+    while ((byte = getc(input_f)) != EOF)
+    {
+        if (i != 0) fprintf(output_f, ", ");
+        fprintf(output_f, "0x%02X", byte);
+        ++i;
+    }
+
+    if (i != 0) fprintf(output_f, ", ");
+    fprintf(output_f, "0x00");
+    ++i;
+
+    fprintf(output_f, "\n        };\n");
+
+    fprintf(output_f, "        return d;\n    }\n");
+
+    fprintf(output_f, "    static const char* path()\n    {\n");
+    fprintf(output_f, "        return \"");
+
+    for (const auto& it : namespaces)
+        fprintf(output_f, "%s/", it.c_str());
+    fprintf(output_f, "%s.fbs\"", className.c_str());
+    fprintf(output_f, ";\n    }\n");
+
+    fprintf(output_f, "    static const char* directory()\n    {\n");
+    fprintf(output_f, "        return \"");
+
+    for (const auto& it : namespaces)
+        fprintf(output_f, "%s/", it.c_str());
+    fprintf(output_f, "\";\n    }\n");
+
+    fprintf(output_f, "};\n\n");
+
+    for (const auto& it : namespaces)
+        fprintf(output_f, "}\n");
+    fprintf(output_f, "\n#endif\n");
+
+    result = EXIT_SUCCESS;
+exit:
+    if (input_f) fclose(input_f);
+    if (output_f) fclose(output_f);
+
+    return result;
+}
