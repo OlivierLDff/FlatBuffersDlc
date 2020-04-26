c9fc4db6b33b1a15442f44351741843931aaa2e3
Olivier LDff <olivier.ldff@gmail.com>
2020-04-26 10:56:05 +0200

    generateTextFromTable

--- include/flatbuffers/json_parser.h
+++ include/flatbuffers/json_parser.h
@@ -22,6 +22,8 @@ public:
     virtual void setStrictJson(const bool strictJson) = 0;
     virtual int indentStep() const = 0;
     virtual void setIndentStep(const int indentStep) = 0;
+    virtual bool outputDefaultValues() const = 0;
+    virtual void setOutputDefaultValues(const bool outputDefaultValues) = 0;
 
     virtual bool parse(const char* json) = 0;
     virtual std::string error() const = 0;
@@ -31,9 +33,80 @@ public:
 
     virtual const void* root() const = 0;
     virtual bool isValid() const = 0;
-    virtual void reset() = 0;
 
+    /**
+     * Clear the parser and reset the config
+     */
+    virtual void reset() = 0;
+    /** Only reset the config. */
+    virtual void resetConfig() = 0;
+
+    /**
+     * \brief Generate a json formatted string from a row flatbuffer
+     * If you need to dump a raw flatbuffer, use generateTextFromTable.
+     *
+     * Dump a flatbuffers::FlatBuffersBuilder :
+     * \code
+     * // ...
+     * fbb.Finish();
+     *
+     * std::string json;
+     * auto success = parser->generateText(fbb.GetBufferPointer(), json);
+     * if(success)
+     *    std::cout << "json : " << json << std::endl;
+     * else
+     *    std::cerr << "fail to generate json : " << parser->error() << std::endl;
+     * \endcode
+     *
+     * \warning There is no check that flatbuffer is valid buffer, call with care
+     *
+     * \note The output of this function can be customized by changing
+     * - strictJson: if true key are written in between "" otherwise key are raw string. (default true)
+     * - indentStep: If -1, no line break. Otherwise line break + indentation in space. (default -1)
+     * - outputDefaultValues: Values that are equals to default are going to be write. (default true)
+     *
+     * \param[in] flatbuffer pointer to the flatbuffer. This is the raw data not the table!!
+     * So it's the data received from the network or from fbb.GetBufferPointer()
+     * \param[out] output json output string
+     * \return true if json generation is a success, false otherwise.
+     * You can get any error using error()
+     */
     virtual bool generateText(const void* flatbuffer, std::string& output) = 0;
+
+    /**
+     * \brief Generate a json formatted string from flatbuffer table.
+     * If you need to dump a raw flatbuffer, use generateText.
+     *
+     * Dump a Monster.fbs table
+     * \code
+     * // ...
+     * auto monster = GetMonster(buffer);
+     *
+     * std::string json;
+     * auto success = parser->generateTextFromTable(monster, json);
+     * if(success)
+     *    std::cout << "json : " << json << std::endl;
+     * else
+     *    std::cerr << "fail to generate json : " << parser->error() << std::endl;
+     * \endcode
+     *
+     * \warning There is no check that flatbuffer is valid buffer, call with care
+     *
+     * \note The output of this function can be customized by changing
+     * - strictJson: if true key are written in between "" otherwise key are raw string. (default true)
+     * - indentStep: If -1, no line break. Otherwise line break + indentation in space. (default -1)
+     * - outputDefaultValues: Values that are equals to default are going to be write. (default true)
+     *
+     * \note This function require for the generated flatbuffer code to use --reflect-types or --reflect-names
+     *
+     * \param[in] table pointer to the flatbuffer. This is the raw data not the table!!
+     * So it's the data received from the network or from fbb.GetBufferPointer()
+     * \param[out] output json output string
+     * \return true if json generation is a success, false otherwise.
+     * You can get any error using error()
+     */
+    virtual bool generateTextFromTable(
+        const void* table, std::string& output) = 0;
 };
 
 /**
@@ -47,11 +120,11 @@ public:
 template<class T, class... Types>
 class TJsonParser : public JsonParser
 {
-    static_assert(sizeof...(Types) >= 1, "TJsonParser required at least 1 generated resource class to parser T");
+    static_assert(sizeof...(Types) >= 1,
+        "TJsonParser required at least 1 generated resource class to parser T");
+
 public:
-    TJsonParser() : _parser(std::make_unique<Parser>())
-    {
-    }
+    TJsonParser() : _parser(std::make_unique<Parser>()) {}
 
 private:
     std::unique_ptr<Parser> _parser;
@@ -59,22 +132,22 @@ private:
     bool _strictJson = true;
     bool _outputDefaultValues = true;
     int _indentStep = -1;
-    static std::shared_ptr<TJsonParser<T, Types ...>> _instance;
+    static std::shared_ptr<TJsonParser<T, Types...>> _instance;
 
 private:
     bool initializeParser()
     {
-        if (_initialized)
+        if(_initialized)
             return true;
 
         constexpr auto size = sizeof...(Types);
 
-        static const char* data[size] = { Types::data()... };
-        static const char* path[size] = { Types::path()... };
+        static const char* data[size] = {Types::data()...};
+        static const char* path[size] = {Types::path()...};
 
-        for (auto i = 0; i < size; ++i)
+        for(auto i = 0; i < size; ++i)
         {
-            if (!_parser->Parse(data[i], nullptr, path[i]))
+            if(!_parser->Parse(data[i], nullptr, path[i]))
             {
                 // This flag mean the parser will never be able to work again, it need a fix in the code
                 // Program should crash and parser need to be fixed by developer !!!
@@ -85,33 +158,28 @@ private:
         _initialized = true;
         return true;
     }
+
 public:
-    bool strictJson() const override final
-    {
-        return _strictJson;
-    }
+    bool strictJson() const override final { return _strictJson; }
 
     void setStrictJson(const bool strictJson) override final
     {
         _strictJson = strictJson;
     }
 
-    int indentStep() const override final
-    {
-        return _indentStep;
-    }
+    int indentStep() const override final { return _indentStep; }
 
     void setIndentStep(const int indentStep) override final
     {
         _indentStep = indentStep < 0 ? -1 : indentStep;
     }
 
-    bool outputDefaultValues() const
+    bool outputDefaultValues() const override final
     {
         return _outputDefaultValues;
     }
 
-    void setOutputDefaultValues(const bool outputDefaultValues)
+    void setOutputDefaultValues(const bool outputDefaultValues) override final
     {
         _outputDefaultValues = outputDefaultValues;
     }
@@ -119,7 +187,7 @@ public:
     bool parse(const char* json) override final
     {
         // Init parser
-        if (!initializeParser())
+        if(!initializeParser())
             return false;
 
         // Set parser options
@@ -131,37 +199,75 @@ public:
 
     std::string error() const override final { return _parser->error_; }
 
-    const std::uint8_t* buffer() const override final { return _parser->builder_.GetBufferPointer(); }
-    std::size_t size() const override final { return _parser->builder_.GetSize(); }
+    const std::uint8_t* buffer() const override final
+    {
+        return _parser->builder_.GetBufferPointer();
+    }
+    std::size_t size() const override final
+    {
+        return _parser->builder_.GetSize();
+    }
 
-
-    const void* root() const override final { return buffer() ? flatbuffers::GetRoot<T>(buffer()) : nullptr; }
-    const T* flatbuffer() const { return buffer() ? flatbuffers::GetRoot<T>(buffer()) : nullptr; }
+    const void* root() const override final
+    {
+        return buffer() ? flatbuffers::GetRoot<T>(buffer()) : nullptr;
+    }
+    const T* flatbuffer() const
+    {
+        return buffer() ? flatbuffers::GetRoot<T>(buffer()) : nullptr;
+    }
     bool isValid() const override final
     {
         return Verifier(buffer(), size()).VerifyBuffer<T>(nullptr);
     }
 
-    void reset() override final { _parser = std::make_unique<Parser>(); _initialized = false; }
+    void reset() override final
+    {
+        _parser = std::make_unique<Parser>();
+        _initialized = false;
+        resetConfig();
+    }
 
-    bool generateText(const void* flatbuffer, std::string& output) override final
+    void resetConfig() override final
+    {
+        _strictJson = true;
+        _outputDefaultValues = true;
+        _indentStep = -1;
+    }
+
+private:
+    bool initGenerateText()
     {
         // Init parser
-        if (!initializeParser())
+        if(!initializeParser())
             return false;
 
         // Set parser options
         _parser->opts.strict_json = _strictJson;
         _parser->opts.indent_step = _indentStep;
         _parser->opts.output_default_scalars_in_json = _outputDefaultValues;
-
-        // Generate text
-        return GenerateText(*_parser, flatbuffer, &output);
+        return true;
     }
 
 public:
-    static std::shared_ptr<TJsonParser<T, Types ...>> make() { return std::make_shared<TJsonParser<T, Types ...>>(); }
-    static std::shared_ptr<TJsonParser<T, Types ...>> get()
+    bool generateText(
+        const void* flatbuffer, std::string& output) override final
+    {
+        return initGenerateText() &&
+               GenerateText(*_parser, flatbuffer, &output);
+    }
+    bool generateTextFromTable(
+        const void* table, std::string& output) override final
+    {
+        return initGenerateText() && GenerateTextFromTable(*_parser, table,
+                                         T::GetFullyQualifiedName(), &output);
+    }
+
+    static std::shared_ptr<TJsonParser<T, Types...>> make()
+    {
+        return std::make_shared<TJsonParser<T, Types...>>();
+    }
+    static std::shared_ptr<TJsonParser<T, Types...>> get()
     {
         // Always return an instance that isn't used in another thread or another context.
         // This will avoid thread safe issue
@@ -169,11 +275,11 @@ public:
             _instance = make();
         return _instance;
     }
-
 };
 
 template<class T, class... Types>
-std::shared_ptr<TJsonParser<T, Types ...>> TJsonParser<T, Types ...>::_instance = TJsonParser<T, Types ... >::make();
+std::shared_ptr<TJsonParser<T, Types...>> TJsonParser<T, Types...>::_instance =
+    TJsonParser<T, Types...>::make();
 
 }
 
